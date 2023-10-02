68c2a7273f4d6b7a93c77e428fe7fc5d5cfe6c0a
Mogyoródi Balázs <mogyorodi.balazs@dk.akkszalezi.hu>
2023-10-02 13:27:53 +0200

    hibás betöltés és félig kész scannelés

--- src/teszt/Main.java
+++ src/teszt/Main.java
@@ -1,20 +1,72 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package teszt;
 
-/**
- *
- * @author mogyorodi.balazs
- */
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    static String [] pakli = new String[22];
+    static  Scanner src = new Scannner();
+
+    public static void main(String[] args) {
+
+        feltoltes();
+        kirak();
+        for (int i = 0; i < 3; i++) {
+            int oszlop = melyik();
+            kever();
+            kirak();
+        }
+        ezVolt();
+
+    }
+
+    private static void feltoltes() {
+        String [] szinek = {"Z", "K", "ZS", "D"};
+        String [] ertek = { "VIII", "IX", "X", "J", "K", "A"};
+        
+        for (String szin : szinek) {
+             int hanyadik = 0;
+            for (int i = 1; i < 22 && hanyadik < ertek.length; i++) {
+                pakli[i] = szin + "_" + ertek[hanyadik];   
+                hanyadik ++;
+            }      
+        }
+        
+        
+        
+    }
+
+    private static void kirak() {
+      
+        for (int i = 1; i < pakli.length; i++) {
+            System.out.print(pakli[i]);
+            if (i%3 == 0) {
+                System.out.println("");
+            }
+        }
+   
+        
+        
+    }
+
+    private static int melyik() {
+        int melyik;
+        do{
+            src.nextI
+            
+        
+        }while (melyik < 0 && melyik > 3)           
+            
+       
+        
+        
+        
+        return melyik;
+    }
+
+    private static void kever() {
+
+    }
+
+    private static void ezVolt() {
+    }
+
 }
